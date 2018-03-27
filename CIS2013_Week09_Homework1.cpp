81c19033f5c953ad7675fd73eba893f5a9550970
sonnyicks <35547157+sonnyicks@users.noreply.github.com>
2018-03-26 20:58:59 -0500

    instream file

--- CIS2013_Week09_Homework1.cpp
+++ CIS2013_Week09_Homework1.cpp
@@ -5,7 +5,20 @@
 using namespace std;
 
 int main(){
-	cout << "Enter file you want to use to encrypt: ";
+	ifstream encrypt;
+	char file [16];
+	bool valid_file = false;
+	
+	
+	while (valid_file==false){
+	cout << "Enter file name: ";
+	cin >> file;
+	encrypt.open (file);
+		if (encrypt.fail()){
+		cout << "File not available.  Please choose another: ";
+		}
+	else {valid_file=true;}
+	}
 	
 	return 0;
 }
